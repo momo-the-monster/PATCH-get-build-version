f878fcef055edcea4e308b6b7db4dbf11874dd8d
Momo the Monster <momodmonster@gmail.com>
2021-09-15 15:04:39 -0700

    action ready for testing

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-get-latest-version-from-patch-build-index
+# Get PATCH Build Version Action
+
+This action returns the latest version in a Builds Index file as a string when using [PATCH](https://github.com/emanzione/PATCH)
+
+## Inputs
+
+## `pathToBuildsIndex`
+
+The path to the actual builds_index.json file, defaults to "PATCHWorkspace/Builds/builds_index.json"
+
+## Outputs
+
+## `version`
+
+The latest version found, as a string
+
+## Example usage
+
+uses: actions/get-latest-version-from-patch-build-index@v0.1
+with:
+    pathToBuildsIndex: 'PATCHWorkspace/Builds/builds_index.json'
---
+++ index.js
@@ -0,0 +1,35 @@
+﻿const core = require('@actions/core');
+const github = require('@actions/github');
+const fs = require('fs');
+
+try {
+    // path input defined in action metadata file
+    const pathToBuildsIndex = core.getInput('pathToBuildsIndex');
+    console.log(`Got path to builds index as ${pathToBuildsIndex}!`);
+    
+    fs.readFile(pathToBuildsIndex, "utf8", (err, jsonString) => {
+        if (err) {
+            console.log("Error reading file from disk:", err);
+            return;
+        }
+        try {
+            const buildIndex = JSON.parse(jsonString);
+            const builds = buildIndex.AvailableBuilds;
+            console.log(`found ${builds.length} builds`);
+            const latestVersion = builds[builds.length-1];
+            console.log(`latest build version is ${latestVersion}`);
+            
+            core.setOutput("version", latestVersion);
+        } catch (err) {
+            console.log("Error parsing JSON string:", err);
+        }
+    });
+    
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+    
+    
+} catch (error) {
+    core.setFailed(error.message);
+}
